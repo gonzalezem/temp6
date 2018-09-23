83b7d59d0ae4b23c489837ae6f994f49b8356bcc
Emmanuel Gonzalez <manu1617@hotmail.com>
2018-09-23 09:56:22 -0400

    Add files via upload

---
+++ data/destinyModuleOTUList.js
@@ -0,0 +1,1 @@
+var destinyModuleOTUs = ['Bacteroidales_MF_7', 'Bacteroidetes_MG_4', 'Bordetella_1', 'Brevundimonas_MS_2', 'Comamonadaceae_MG_12', 'Cryptomonadaceae_12', 'Helicobacter_typhlonius_1', 'Lactobacillus_4', 'Lactobacillus_7', 'Lactobacillus_florum_1', 'Polynucleobacter_duraquae_1', 'Rhodoferax_MS_1', 'Unknown_bacteria_56'];
